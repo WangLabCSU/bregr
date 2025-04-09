8051d50741e09328428a9a5f7b2ac9a8ae92191a
Shixiang Wang <wangshx@csu.edu.cn>
2025-04-09 18:08:37 +0800

    reinit the project for v2

---
+++ data-raw/lung2.R
@@ -0,0 +1,6 @@
+# https://r-pkgs.org/data.html#sec-data-data
+# tools::R_user_dir() or rappdirs for settin/accessing users' local data
+
+## code to prepare `lung2` dataset goes here
+
+usethis::use_data(lung2, overwrite = TRUE)
